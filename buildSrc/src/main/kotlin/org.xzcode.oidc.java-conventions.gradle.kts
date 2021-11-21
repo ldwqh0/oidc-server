15a7687f66d7022dec12e8cd9e8022eadb5e532d
ldwqh0 <ldwqh0@outlook.com>
2021-11-21 22:00:49 +0800

    修改构建脚本

--- buildSrc/src/main/kotlin/org.xzcode.oidc.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/org.xzcode.oidc.java-conventions.gradle.kts
@@ -14,16 +14,40 @@ plugins {
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 java.targetCompatibility = JavaVersion.VERSION_1_8
 
-
 dependencyManagement {
   imports {
     mavenBom("org.springframework.boot:spring-boot-dependencies:2.5.3")
   }
 }
 
+java {
+  withSourcesJar()
+}
+
 publishing {
   publications.create<MavenPublication>("maven") {
     from(components["java"])
+    versionMapping {
+      usage("java-api") {
+        fromResolutionOf("runtimeClasspath")
+      }
+      usage("java-runtime") {
+        fromResolutionResult()
+      }
+    }
+  }
+  repositories {
+    maven {
+      val releasesRepoUrl = "http://demo.yzhxh.com:8081/nexus/repository/maven-releases/"
+      val snapshotsRepoUrl = "http://demo.yzhxh.com:8081/nexus/repository/maven-snapshots/"
+      url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+      isAllowInsecureProtocol = true
+      credentials {
+        username = "lidong"
+        password = "lidong"
+      }
+    }
+    mavenLocal()
   }
 }
 
